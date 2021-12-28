3a8cd6a7fcda40ffad31d56733f2c5155bfcbdbe
Gabriel Manfroi <32476134+gabemanfroi@users.noreply.github.com>
2021-12-28 14:15:13 -0300

    🚀 deploy stuffs

--- modules/Shared/util/HttpClient.js
+++ modules/Shared/util/HttpClient.js
@@ -4,7 +4,7 @@ import { camelizeKeys } from 'humps';
 class HttpClient {
   constructor() {
     this.axiosInstance = axios.create({
-        baseURL: process.env.NEXT_PUBLIC_BASE_URL,
+        baseURL: process.env.BASE_URL,
         headers: {
           'Content-Type': 'application/json',
         },
